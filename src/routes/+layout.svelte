6681a0952d54984b7920a7749b269fd01465083e
Emiel <emiel.delaey@student.howest.be>
2023-06-22 21:40:48 +0200

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+	import '../app.css';
+    import { themeChange } from 'theme-change'
+    import Header from "./Header.svelte";
+
+    onMount(() => {
+        themeChange(false)
+
+    })
+</script>
+<Header/>
+<slot/>
+
---
+++ src/routes/Header.svelte
@@ -0,0 +1,4 @@
+<header class="flex justify-between px-8 py-4 bg-base-300">
+    <h3 class="text-3xl font-bold inline">Mealplanner</h3>
+    <input type="checkbox" class="toggle " checked data-toggle-theme="dark,light" data-act-class="ACTIVECLASS"/>
+</header>
