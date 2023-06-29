499726df2a818c58c752d1098a584599e377136b
Emiel <emiel.delaey@student.howest.be>
2023-06-29 14:30:09 +0200

    fixed wrong character in schemas.ts

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const productSchema = z.object({
-    name: z.string().min(1).max(20).,
+    name: z.string().min(1).max(20),
     description: z.string().min(1).max(100),
     isFresh: z.boolean(),
     isCanned: z.boolean(),
